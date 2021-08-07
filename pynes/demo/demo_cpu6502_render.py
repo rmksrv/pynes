59ab1382186a13f92c8fe7c7b4413382279ec237
Roman Kosarev <rmksrv@outlook.com>
2021-08-07 14:22:28 +0300

    moved drawing elems to separate methods

--- pynes/demo/demo_cpu6502_render.py
+++ pynes/demo/demo_cpu6502_render.py
@@ -51,50 +51,66 @@ class DemoCpu6502Render:
                 if event.type == pg.QUIT:
                     running = False
 
-            # items preparing
             # background
             screen.fill(Colors.BLACK.value)
-            # ram
-            # cpu status
-            status_label = font.render("status: ", False, Colors.WHITE.value)
-            flag_labels = list()
-            for fname in reversed(FLAGS):
-                if fname == 'u':
-                    fname = '-'
-                    color = Colors.GREEN
-                else:
-                    flag = self.bus.get_cpu6502().get_flag(fname)
-                    color = Colors.GREEN if flag else Colors.RED
-                flag_labels.append(font.render(fname, False, color.value))
-            # pc, regs and sp
-            pc_text = '$' + hex(self.bus.get_cpu6502().pc.value)[2:].zfill(4)
-            pc_label = font.render(f"pc:     {pc_text}", False, Colors.WHITE.value)
-            a_text = '$' + hex(self.bus.get_cpu6502().a.value)[2:].zfill(2)
-            a_label = font.render(f"a:      {a_text}", False, Colors.WHITE.value)
-            x_text = '$' + hex(self.bus.get_cpu6502().x.value)[2:].zfill(2)
-            x_label = font.render(f"x:      {x_text}", False, Colors.WHITE.value)
-            y_text = '$' + hex(self.bus.get_cpu6502().y.value)[2:].zfill(2)
-            y_label = font.render(f"y:      {y_text}", False, Colors.WHITE.value)
-            sp_text = '$' + hex(self.bus.get_cpu6502().sp.value)[2:].zfill(2)
-            sp_label = font.render(f"sp:     {sp_text}", False, Colors.WHITE.value)
-
-            # items positioning
-            # cpu status
-            screen.blit(status_label, (self.width - 200, 10))
-            for i, l in enumerate(flag_labels):
-                screen.blit(l, (self.width - 135 + 15 * i, 10))
-            # pc, regs and sp
-            screen.blit(pc_label, (self.width - 200, 25))
-            screen.blit(a_label, (self.width - 200, 40))
-            screen.blit(x_label, (self.width - 200, 55))
-            screen.blit(y_label, (self.width - 200, 70))
-            screen.blit(sp_label, (self.width - 200, 85))
+            # draw info
+            self.render_status(screen, font)
+            self.render_pc(screen, font)
+            self.render_a(screen, font)
+            self.render_x(screen, font)
+            self.render_y(screen, font)
+            self.render_sp(screen, font)
 
             # upd screen
             pg.display.flip()
 
         pg.font.quit()
         pg.quit()
+
+    def render_ram(self, screen: pg.display, font: pg.font.Font) -> None:
+        pass
+
+    def render_status(self, screen: pg.display, font: pg.font.Font) -> None:
+        # preparing
+        status_label = font.render("status: ", False, Colors.WHITE.value)
+        flag_labels = list()
+        for fname in reversed(FLAGS):
+            if fname == 'u':
+                fname = '-'
+                color = Colors.GREEN
+            else:
+                flag = self.bus.get_cpu6502().get_flag(fname)
+                color = Colors.GREEN if flag else Colors.RED
+            flag_labels.append(font.render(fname, False, color.value))
+        # positioning
+        screen.blit(status_label, (self.width - 200, 10))
+        for i, l in enumerate(flag_labels):
+            screen.blit(l, (self.width - 135 + 15 * i, 10))
+
+    def render_pc(self, screen: pg.display, font: pg.font.Font) -> None:
+        pc_text = '$' + hex(self.bus.get_cpu6502().pc.value)[2:].zfill(4)
+        pc_label = font.render(f"pc:     {pc_text}", False, Colors.WHITE.value)
+        screen.blit(pc_label, (self.width - 200, 25))
+
+    def render_a(self, screen: pg.display, font: pg.font.Font) -> None:
+        a_text = '$' + hex(self.bus.get_cpu6502().a.value)[2:].zfill(2)
+        a_label = font.render(f"a:      {a_text}", False, Colors.WHITE.value)
+        screen.blit(a_label, (self.width - 200, 40))
+
+    def render_x(self, screen: pg.display, font: pg.font.Font) -> None:
+        x_text = '$' + hex(self.bus.get_cpu6502().x.value)[2:].zfill(2)
+        x_label = font.render(f"x:      {x_text}", False, Colors.WHITE.value)
+        screen.blit(x_label, (self.width - 200, 55))
+
+    def render_y(self, screen: pg.display, font: pg.font.Font) -> None:
+        y_text = '$' + hex(self.bus.get_cpu6502().y.value)[2:].zfill(2)
+        y_label = font.render(f"y:      {y_text}", False, Colors.WHITE.value)
+        screen.blit(y_label, (self.width - 200, 70))
+
+    def render_sp(self, screen: pg.display, font: pg.font.Font) -> None:
+        sp_text = '$' + hex(self.bus.get_cpu6502().sp.value)[2:].zfill(2)
+        sp_label = font.render(f"sp:     {sp_text}", False, Colors.WHITE.value)
+        screen.blit(sp_label, (self.width - 200, 85))
 
     @staticmethod
     def get_prepared_bus() -> Bus:
